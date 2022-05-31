7ad0e6df8255afc608f27ee5e8233b37f1b11506
johanesharyoseno <105377208+johanesharyoseno@users.noreply.github.com>
2022-06-01 00:14:44 +0700

    Delete asynchellobinarian.js

--- asynchellobinarian.js
+++
@@ -1,9 +0,0 @@
-//synchronous
-console.log ("halo");
-console.log ("selamat pagi");
-console.log ("johanes");
-
-//asynchoronous
-console.log ("Hello binarian");
-setTimeout (() => {console.log("javascript")},5000);
-console.log ("developer")
